7699a70f6220179b78398b5f96dd3dde744b1e0c
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-08-05 22:21:38 +0200

    FieldEdit component should load field events

--- src/app/fields/field-edit/field-edit.component.ts
+++ src/app/fields/field-edit/field-edit.component.ts
@@ -22,9 +22,11 @@ export class FieldEditComponent implements OnInit, OnDestroy {
   isEditMode = false;
   fieldForm: FormGroup;
   clients: Client[] = [];
+  events: Event[] = [];
 
   private fieldStoreSub: Subscription;
   private clientStoreSub: Subscription;
+  private eventStoreSub: Subscription;
 
   get eventsControls() {
     return this.getEvents().controls;
@@ -64,8 +66,16 @@ export class FieldEditComponent implements OnInit, OnDestroy {
     // this.router.navigate(["../"], { relativeTo: this.route });
   }
 
-  onManageField() {
-    // this.router.navigate(["../", "fields"], { relativeTo: this.route });
+  onAddEvent() {
+    this.getEvents().push(
+      new FormGroup({
+        eventType: new FormControl(null, Validators.required),
+        date: new FormControl(null, [
+          Validators.required
+          // Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
+      })
+    );
   }
 
   onCancel() {
@@ -85,6 +95,9 @@ export class FieldEditComponent implements OnInit, OnDestroy {
     }
     if (this.clientStoreSub) {
       this.clientStoreSub.unsubscribe();
+    }
+    if (this.eventStoreSub) {
+      this.eventStoreSub.unsubscribe();
     }
   }
 
@@ -108,25 +121,11 @@ export class FieldEditComponent implements OnInit, OnDestroy {
     let name = "";
     let description = "";
     let email = "";
-    let events: Event[] = [];
+    let clientEvents: Event[] = [];
     let client = -1;
 
     this.store.dispatch(ClientActions.fetchClients());
-    this.clientStoreSub = this.store
-      .select("client")
-      .pipe(
-        map(clientState => {
-          return clientState.clients.filter(c => {
-            return c.active;
-          });
-        }),
-        tap(results => {
-          results.sort();
-        })
-      )
-      .subscribe(activeClients => {
-        this.clients = activeClients;
-      });
+    this.loadClients();
 
     if (this.isEditMode) {
       this.fieldStoreSub = this.store
@@ -142,19 +141,59 @@ export class FieldEditComponent implements OnInit, OnDestroy {
           name = editedField.name;
           description = editedField.description;
           email = editedField.email;
-          events = editedField.events;
+          clientEvents = editedField.events;
           client = this.clients
             .map(c => c._id.toString())
             .indexOf(editedField.client.toString());
         });
+
+      if (clientEvents.length > 0) {
+        this.loadEvents();
+      }
     }
 
     this.fieldForm = new FormGroup({
       name: new FormControl(name, Validators.required),
       description: new FormControl(description, Validators.required),
       email: new FormControl(email, Validators.required),
-      events: new FormArray(this.createEventsControls(events)),
+      events: new FormArray(this.createEventsControls(clientEvents)),
       client: new FormControl(client, Validators.required)
     });
+  }
+
+  private loadClients() {
+    this.clientStoreSub = this.store
+      .select("client")
+      .pipe(
+        map(clientState => {
+          return clientState.clients.filter(c => {
+            return c.active;
+          });
+        }),
+        tap(results => {
+          results.sort();
+        })
+      )
+      .subscribe(activeClients => {
+        this.clients = activeClients;
+      });
+  }
+
+  private loadEvents() {
+    this.eventStoreSub = this.store
+      .select("event")
+      .pipe(
+        map(eventState => {
+          return eventState.events.filter(c => {
+            return c.active;
+          });
+        }),
+        tap(results => {
+          results.sort();
+        })
+      )
+      .subscribe(activeEvents => {
+        this.events = activeEvents;
+      });
   }
 }
