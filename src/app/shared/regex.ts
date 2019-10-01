b70f24997112e6e3996e5b2e55c9c578fa392636
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-10-01 23:23:09 +1000

    Added latin chars to regex

--- src/app/shared/regex.ts
+++ src/app/shared/regex.ts
@@ -1,5 +1,5 @@
-const TEXT = /^[a-zA-Z0-9 ]+$/;
-const TEXT_NO_EMPTY_SPACE = /^[a-zA-Z0-9]+$/;
+const TEXT = /^([ A-z0-9!@#\$%\^\&*\)\(+=._-\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff]*)$/;
+const TEXT_NO_EMPTY_SPACE = /^([A-z0-9!@#\$%\^\&*\)\(+=._-\u00C0-\u00D6\u00D8-\u00f6\u00f8-\u00ff]*)$/;
 const EMAIL = /\S+@\S+\.\S+/;
 const POSTAL_CODE = /^[0-9]{5}[-][0-9]{3}$/;
 const INTEGER = /^[0-9]+$/;
